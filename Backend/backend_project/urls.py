ee26ad6d6ffbe8b62a3b7243d049be03fe788b54
Jhon Huanacuni Mamani <jhon.huanacuni.mamani@gmail.com>
2026-08-04 13:07:29 -0500

    fix

--- Backend/backend_project/urls.py
+++ Backend/backend_project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
@@ -14,5 +15,12 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
+# PDFs/imágenes subidos (biblioteca, etc.). En producción Apache puede servir /media/
+# directamente (ver deploy/academia.usercodex.com.conf); este fallback evita 404 si aún
+# se hace proxy a Gunicorn.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
